4a3c05aca8cc680355b495c6cf1ddf6c425951de
Simon <simon.taranto@gmail.com>
2013-07-08 19:41:20 -0600

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FindCoffee::Application.config.secret_token = '3a0030ffc985725952658b21392ae39fe61ac4f19c446ad380f27dd841234f2ffaf252dd27dfe46691defdc2016d56e28228be7bc468edf9eaa569aae8c7cbd4'
